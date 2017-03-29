6670ebdb9d5329d4882cbf3de66d0ae9ae884c40
spetsing <spetsing@gmail.com>
2017-03-29 13:06:46 -0400

    Finished HTML for login and register peices. LoginController has functions mapped out

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # IP-FinalProject
-asdf
+
+1) Download Git https://git-scm.com/
+    - When installing, select option to use both regular command line and git bash
+2) Download NPM (Node Package Manager) https://nodejs.org/en/
+    - NPM allows us to install the 3rd party dependencies we will need for our Node.js sever
+3) Create a new folder to house the project
+
+4) Right click inside of new folder and select open git bash
+
+5) In the git bash console, enter the following commands in the order seen below:
+    - git clone https://github.com/spetsing/IP-FinalProject.git
+    - cd IP-FinalProject
+    - npm install
+    - npm install bower -g
+    - bower install
+    
+6) All your dependencies should now be installed. You will need to run our node server.js to test the app
+
+7) Navigate to the folder that holds server.js and type this git bash command to start the server
+	-node server.js
+	- you should see a message that the server is running on port 8080
+
+8) Open your browser and navigate to http://localhost:8080/
--- public/js/controllers/LoginController.js
+++ public/js/controllers/LoginController.js
@@ -1,10 +1,74 @@
 var module = angular.module('myApp');
 
 function LoginController($scope, Services) {
+    /*
+        Login: Make rest call to server. Verfiy username and password. If valid, route to main page and save user info in model
+    */
+    $scope.login = function () {
+        console.log("LOGGIN");
+        //save user info to global model
+
+        //route to main page
+    }
+
+    /*
+        Sign Up New User: Hide loginDiv and show signUpDiv
+    */
+    $scope.signUp = function () {
+        console.log("SIGNUP");
+        $("#loginDiv").hide();
+        $("#signUpDiv").show();
+        $("#classIdDiv").hide();
+    }
+
+    /*
+        Register New User
+    */
+    $scope.register = function() {
+        var user = {
+            firstName: document.getElementById("register_firstName").value,
+            lastName: document.getElementById("register_lastName").value,
+            userName: document.getElementById("register_username").value,
+            password: document.getElementById("register_password").value,
+            classID:  document.getElementById("register_classID").value
+        }
+        console.log(user);
+    }
+
+    /*
+        Cancel Sign Up: Clear Fields, Hide signUpDiv and show loginDiv
+    */
+    $scope.cancel = function() {
+        document.getElementById("register_firstName").value = ""
+        document.getElementById("register_lastName").value = ""
+        document.getElementById("register_username").value = ""
+        document.getElementById("register_password").value = ""
+        document.getElementById("register_classID").value = ""
+        $("#loginDiv").show();
+        $("#signUpDiv").hide();
+    }
+
+    $scope.selectTeacher = function($event) {
+        var checkBox = document.getElementById("teacherCheckBox");
+        if(checkBox.checked === true) {
+            $("#classIdDiv").show();
+        } else {
+            $("#classIdDiv").hide();
+        }
+    }
+
+    /*
+        Runs when html is rendered. Hide nav bar and signUpDiv
+    */
+    $(document).ready(function () {
+        $("#navBar").hide();
+        $("#signUpDiv").hide();
+    });
 
 
 
 }
+
 
 LoginController.$inject = [
         '$scope',
