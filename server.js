041f577bd565a41772a3b10f47dd0f4cf6da047d
spetsing <spetsing@gmail.com>
2017-04-12 12:35:36 -0400

    Added mongodb connection to master

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-
 var routes = require('./app/routes');
+var mongoCredentials = require('./mongocredentials');
+var mongoURL = 'mongodb://'+ mongoCredentials.dbName + ':' + mongoCredentials.password + '@127.0.0.1:27017/ip';
+var mongoose = require("mongoose");
+var db = mongoose.connection;
+
+mongoose.connect(mongoURL);
+//Called when there is an error connecting to mongoDB
+db.on('error', console.error.bind(console, 'connection error:'));
+//Called when successfully connected to MongoDB
+db.once('open', function callback () {
+  console.log('DB connection opened');
+});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
