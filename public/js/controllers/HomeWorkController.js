c34799ff0a153c92b140d18d7ef33b7e8e06497e
spetsing <spetsing@gmail.com>
2017-04-19 12:43:00 -0400

    Start to wishlist

--- public/js/controllers/HomeWorkController.js
+++ public/js/controllers/HomeWorkController.js
@@ -2,32 +2,35 @@ var module = angular.module('myApp');
 
 function HomeWorkController($scope, Services) {
     $scope.services = Services;
-    $scope.homework = [{date:"10-1",description:"test"}];
+    $scope.homework = [{
+        date: "10-1",
+        description: "test"
+    }];
     $scope.classID = Services.getUserInfo().class.id;
     var isTeacher = Services.isTeacher();
 
     //Check if teacher. If Not, hide ability to add Homework
     $(document).ready(function () {
 
-        if(this.services.isTeacher()){
+        if (this.services.isTeacher()) {
             debugger;
-           }
+        }
     }.bind($scope));
 
     $.ajax({
-                type: 'GET',
-                url: 'http://34.195.93.38:3001/getHomework?id='+$scope.classID,
-                success: function (data) {
-                    this.homework = data;
-                    this.$digest();
-                }.bind($scope),
-                error: function (err) {
-                    alert(err.responseJSON);
-                }.bind(this)
-            });
+        type: 'GET',
+        url: 'http://34.195.93.38:3001/getHomework?id=' + $scope.classID,
+        success: function (data) {
+            this.homework = data;
+            this.$digest();
+        }.bind($scope),
+        error: function (err) {
+            alert(err.responseJSON);
+        }.bind(this)
+    });
 
 
-    $scope.addHomework = function() {
+    $scope.addHomework = function () {
         var date = $("#hwInputDate").val();
         var description = $("#hwInputDescription").val();
 
@@ -38,25 +41,20 @@ function HomeWorkController($scope, Services) {
         }
 
         $.ajax({
-                type: 'POST',
-                url: 'http://34.195.93.38:3001/addHomework',
-                data: homework,
-                success: function (data) {
-                    this.homework.push(homework);
-                    this.$digest();
-                    $("#hwInputDate").val("");
-                    $("#hwInputDescription").val("");
-                }.bind(this),
-                error: function (err) {
-                    alert(err.responseJSON);
-                }.bind(this)
-            });
+            type: 'POST',
+            url: 'http://34.195.93.38:3001/addHomework',
+            data: homework,
+            success: function (data) {
+                this.homework.push(homework);
+                this.$digest();
+                $("#hwInputDate").val("");
+                $("#hwInputDescription").val("");
+            }.bind(this),
+            error: function (err) {
+                alert(err.responseJSON);
+            }.bind(this)
+        });
     }
-
-
-
-
-
 }
 
 HomeWorkController.$inject = [
--- public/js/controllers/WishListController.js
+++ public/js/controllers/WishListController.js
@@ -2,6 +2,70 @@ var module = angular.module('myApp');
 
 function WishListController($scope, Services) {
 
+    $scope.wishList = [{
+        id: "test",
+        description: "Pencils",
+        parentAssigned:"none"
+    },{id:"test1",description:"Paper Towels",parentAssigned:"Dorthy"}];
+    $scope.services = Services;
+    $scope.classID = Services.getUserInfo().class.id;
+
+    //Check if teacher. If Not, hide ability to add Homework
+    $(document).ready(function () {
+        if (this.services.isTeacher()) {
+            debugger;
+        }
+    }.bind($scope));
+
+    $.ajax({
+        type: 'GET',
+        url: 'http://34.195.93.38:3001/getWishList?id=' + $scope.classID,
+        success: function (data) {
+            this.wishList = data;
+            this.$digest();
+        }.bind($scope),
+        error: function (err) {
+            alert(err.responseJSON);
+        }.bind(this)
+    });
+
+
+    $scope.addToDo = function () {
+
+        var description = $("#ToDOInputDescription").val();
+
+        var wishListItem = {
+            classID: this.classID,
+            id: new Date().toISOString(),
+            userName: this.services.getUserInfo().userName,
+            markedCompleted: [],
+            date: new Date().toDateString(),
+            description: description,
+            parentAssigned: "none"
+        }
+
+        $.ajax({
+            type: 'POST',
+            url: 'http://34.195.93.38:3001/addWishList',
+            data: wishListItem,
+            success: function (data) {
+                this.wishLIst.push(wishListItem);
+                this.$digest();
+                $("#messageInputDescription").val("");
+            }.bind(this),
+            error: function (err) {
+                alert(err.responseJSON);
+            }.bind(this)
+        });
+    }
+
+    $scope.test = function(x) {
+        if(x.parentAssigned === "none"){
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 
 }
